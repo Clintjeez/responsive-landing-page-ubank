f4165e33a738caa5976ab59e3951558cc5a31d43
Clinton James <clintjeez@gmail.com>
2023-01-07 04:28:58 +0100

    custom button component done

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../styles";
+import Button from "./Button";
 
 const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do business, <br className="sm:block hidden" /> we'll handle the
+          money.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. but with hundreds
+          of credit cards on the market.
+        </p>
+        <Button styles="mt-10" />
+      </div>
+    </section>
+  );
 };
 
 export default Business;
